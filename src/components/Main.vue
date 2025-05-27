85a74955a5d545e4f0320b84b36cc86e95db253e
rongxuanzhou <1152424559@qq.com>
2025-05-28 06:40:56 +0800

    Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>Hello World</h1>
+  <a-layout>
+    <a-layout-sider :style="siderStyle">Sider</a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout-content :style="contentStyle">Content</a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script lang="ts">
 export default {
   name: 'Main',
+}
+</script>
+
+<script lang="ts" setup>
+import type { CSSProperties } from 'vue'
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+}
+
+const contentStyle: CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+}
+
+const siderStyle: CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
 }
 </script>
